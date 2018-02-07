d1998fde927bb2680d40ce67214a7a34dafb6d02
Jianhao Tan <jianhaot.89@gmail.com>
2018-02-07 23:06:40 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// different cards in stack
+var cards = ['queen', 'queen', 'king', 'king'];
 
+// cards in play
+var cardsInPlay = [];
+
+// first card user flipped
+var cardOne = cards[0];
 console.log(`user flipped ${cardOne}`);
+
+var cardTwo = cards[1];
+console.log(`user flipped ${cardTwo}`);
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log(cardsInPlay);
+
+// check if 2 cards have been played
+if(cardsInPlay.length === 2){
+  console.log("2 cards have been played");
+  if(cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+  }
+  else{
+    alert("Sorry, try again.");
+  }
+}
+
+// check for equality between 2 cards in cardsInPlay array
